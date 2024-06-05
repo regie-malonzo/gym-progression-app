ec4817049e9062529f49062e4af204148346f7a7
Regie Malonzo <regiemalonzo@gmail.com>
2024-06-06 11:14:59 +1200

    create route and function for getExerciseById

--- server/db/dbFunctions.ts
+++ server/db/dbFunctions.ts
@@ -1,6 +1,10 @@
 import connection from './connection.ts'
 import { Exercise } from '../../models/exercises.ts'
 
-export async function getAllExercises(db = connection): Promise<Exercise[]> {
-  return db('exercises').select('exercise_name')
+export async function getAllExercises(): Promise<Exercise[]> {
+  return await connection('exercises').select('exercise_name')
+}
+
+export async function getExerciseById(id: number) {
+  return await connection('exercises').where({ id }).first()
 }
--- server/routes/exercises.ts
+++ server/routes/exercises.ts
@@ -15,4 +15,15 @@ router.get('/', async (req, res) => {
   }
 })
 
+router.get('/:id', async (req, res) => {
+  try {
+    const id = Number(req.params.id)
+    const exercises = await db.getExerciseById(id)
+    res.json(exercises)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'Something went wrong' })
+  }
+})
+
 export default router
