8ff8589c038c67f1c6b5b1e6e8e68589c7f107f4
Regie Malonzo <regiemalonzo@gmail.com>
2024-07-04 13:28:00 +1200

    create record apis

--- client/apis/Records.ts
+++ client/apis/Records.ts
@@ -1,9 +1,31 @@
 import request from 'superagent'
+import { Record, RecordData } from '../../models/exercises'
 
 const rootUrl = '/api/v1'
 
-export function getFruits(): Promise<string[]> {
-  return request.get(rootUrl + '/fruits').then((res) => {
-    return res.body.fruits
-  })
+export async function getRecords(): Promise<Record[]> {
+  const response = await request.get(rootUrl + 'records')
+  return response.body
+}
+
+export async function getRecordsByExerciseId(
+  exercise_id: number,
+): Promise<RecordData[]> {
+  const response = await request.get(`${rootUrl}records/${exercise_id}`)
+  return response.body
+}
+
+export async function addRecord(record: RecordData): Promise<void> {
+  await request.post(rootUrl + 'records').send(record)
+}
+
+export async function updateRecord(
+  id: number,
+  record: RecordData,
+): Promise<void> {
+  await request.patch(`${rootUrl}records/${id}`).send(record)
+}
+
+export async function deleteRecord(id: number): Promise<void> {
+  await request.delete(`${rootUrl}records/${id}`)
 }
